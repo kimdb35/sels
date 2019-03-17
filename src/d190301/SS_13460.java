13f5217deaf8224cac9b43212324ff8737a4c9fb
Kimddadf[asdfA <kimdb35@naver.com>
2019-03-17 15:56:36 +0900

    algorithm back up

--- src/d190301/SS_13460.java
+++ src/d190301/SS_13460.java
@@ -1,163 +1,224 @@
 package d190301;
 
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class SS_13460 {
 
 	static int V; // 가로
 	static int H; // 세로
-	static int[] r = new int[2];
-	static int[] b = new int[2];
-	static int[] goal = new int[2];
-	static int ans = -1;
+	static int ans = 100;
+	static boolean finished = false;
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
-		String[][] toy;
-		LinkedList<QueDir> rQue = new LinkedList<QueDir>();
-		LinkedList<QueDir> bQue = new LinkedList<QueDir>();
+		String[][] map;
+		
+		Queue<QueDir> que = new LinkedList<>();
 
-		QueDir rqd = new QueDir();
-		QueDir bqd = new QueDir();
 		H = sc.nextInt();
 		V = sc.nextInt();
 
-		toy = new String[H][V];
+		int rx=0, ry=0, bx=0, by=0;
+		map = new String[H][V];
 
 		for (int i = 0; i < H; i++) {
 			String str = sc.next();
 			String[] arr = str.split("");
 			for (int j = 0; j < V; j++) {
-				toy[i][j] = arr[j];
+				map[i][j] = arr[j];
 				if ("R".equals(arr[j])) {
-					rqd = new QueDir(j, i, 99);
+					map[i][j] = ".";
+					rx = j;
+					ry = i;
 				}
 				if ("B".equals(arr[j])) {
-					bqd = new QueDir(j, i, 99);
-				}
-				if ("O".equals(arr[j])) {
-					goal[0] = j;
-					goal[1] = i;
+					map[i][j] = ".";
+					bx = j;
+					by = i;
 				}
 			}
 		}
-		int cnt = 1;
-		while (!rQue.isEmpty()) {
-			if (cnt > 10)
-				break;
-			for (int i = 0; i < 4; i++) {
-				if (isValid(rqd, toy, i) || isValid(bqd, toy, i)) {
-					addDir(i, rqd, rQue);
-					addDir(i, bqd, bQue);
-				}
-			}
+		que.add(new QueDir(rx, ry, bx, by, -1, 1));
+		while (!que.isEmpty()) {
+			QueDir nowInfo = que.poll();
+			int cnt = nowInfo.cnt;
+			int nowRx = nowInfo.getRx();
+			int nowRy = nowInfo.getRy();
+			int nowBx = nowInfo.getBx();
+			int nowBy = nowInfo.getBy();
+			int dir = nowInfo.getDir();
+			if(cnt>10) break;
 			
-			cnt++;
+			for(int i=0; i<4; i++) {
+				if(dir%2!=i%2) moveFirst(i,nowRx, nowRy, nowBx, nowBy, map, que, cnt);
+			}
 		}
 
+		if(ans==100) ans = -1;
 		System.out.println(ans);
 		sc.close();
 	}
-
-	static boolean isValid(QueDir qd, String[][] toy, int idx) {
-		switch(idx) {
-			case 0:
-				if (!toy[qd.y + 1][qd.x].equals("#") && qd.dir != 2)
-					return true;
-				break;
-			case 1:
-				if (!toy[qd.y][qd.x + 1].equals("#") && qd.dir != 3)
-					return true;
-				break;
-			case 2:
-				if (!toy[qd.y - 1][qd.x].equals("#") && qd.dir != 0)
-					return true;
-				break;
-			case 3:
-				if (!toy[qd.y][qd.x - 1].equals("#") && qd.dir != 1)
-					return true;
-				break;
+	
+	static void moveFirst(int dir, int rx, int ry, int bx, int by, String[][] map, Queue<QueDir> que, int cnt) {
+		int[] red = move(dir, rx, ry, map);
+		int[] blue = move(dir, bx, by, map);
+		boolean redBlue = false;
+		if(red[0]==blue[0] && red[1]==blue[1]) {
+			if(finished) redBlue = true;
+			else {
+				switch(dir) {
+					case 0:
+						if(ry>by) blue[1]--;
+						else red[1]--;
+						break;
+					case 1:
+						if(rx>bx) blue[0]--;
+						else red[0]--;
+						break;
+					case 2:
+						if(ry>by) red[1]++;
+						else blue[1]++;
+						break;
+					case 3:
+						if(rx>bx) red[0]++;
+						else blue[0]++;
+						break;
+				}
+			}
 		}
-		return false;
-	}
-
-	static void addDir(int idx, QueDir qd, LinkedList<QueDir> que) {
-		switch (idx) {
-			case 0:
-				que.add(new QueDir(qd.y + 1, qd.x, idx));
-				break;
-			case 2:
-				que.add(new QueDir(qd.y - 1, qd.x, idx));
-				break;
-			case 1:
-				que.add(new QueDir(qd.y, qd.x + 1, idx));
-				break;
-			case 3:
-				que.add(new QueDir(qd.y, qd.x - 1, idx));
-				break;
+		if(redBlue) {									// 둘이 같이 빠진 경우
+			finished = false;
+			return;
+		} else if(!finished) {							// 하나도 안빠진 경우
+			que.add(new QueDir(red[0], red[1], blue[0], blue[1], dir, cnt+1));
+		} else if(finished) {							// 하나만 빠진 경우
+			if(map[red[1]][red[0]].equals("O")) {		// 빨강만 빠진 경우
+				ans = ans>cnt?cnt:ans;
+				return;
+			} else finished = false;
 		}
 	}
 	
-	static boolean move(LinkedList<QueDir> que, String[][] toy, String type) {
-		QueDir qd = que.poll();
-		switch (qd.dir) {
-		case 0:
-			for (int i = qd.y; i < H; i++) {
-				if (toy[i][qd.x].equals("O")) {	// hey!
-					return true;
-				} else if (!toy[i][qd.x].equals(".")) {
-					toy[qd.y][qd.x] = ".";
-					toy[i][qd.x] = type;
-					break;
+	static int[] move(int dir, int x, int y, String[][] map) {
+		int[] cord = new int[2];
+		switch(dir) {
+			case 0:
+				cord[0] = x;
+				for(int i=y; i<H-1; i++) {
+					if(!map[i+1][x].equals(".")) {
+						cord[1] = i;
+						if(map[i+1][x].equals("O")) {
+							cord[1] = i+1;
+							finished = true;
+							return cord;
+						}
+						break;
+					}
 				}
-			}
-			break;
-		case 1:
-			for (int i = qd.x; i < V; i++) {
-				if (toy[qd.y][i].equals("O")) {
-					return true;
-				} else if (!toy[qd.y][i].equals(".")) {
-					toy[qd.y][qd.x] = ".";
-					toy[qd.y][i] = type;
-					break;
+				break;
+			case 1:
+				cord[1] = y;
+				for(int i=x; i<V-1; i++) {
+					if(!map[y][i+1].equals(".")) {
+						cord[0] = i;
+						if(map[y][i+1].equals("O")) {
+							cord[0] = i+1;
+							finished = true;
+							return cord;
+						}
+						break;
+					}
 				}
-			}
-			break;
-		case 2:
-			for (int i = qd.y; i > 0; i--) {
-				if (toy[i][qd.x].equals("O")) {
-					return true;
-				} else if (!toy[i][qd.x].equals(".")) {
-					toy[qd.y][qd.x] = ".";
-					toy[i][qd.x] = type;
-					break;
+				break;
+			case 2:
+				cord[0] = x;
+				for(int i=y; i>0; i--) {
+					if(!map[i-1][x].equals(".")) {
+						cord[1] = i;
+						if (map[i-1][x].equals("O")) {
+							cord[1] = i-1;
+							finished = true;
+							return cord;
+						}
+						break;
+					}
 				}
-			}
-			break;
-		case 3:
-			for (int i = qd.x; i > 0; i--) {
-				if (toy[qd.y][i].equals("O")) {
-					return true;
-				} else if (!toy[qd.y][i].equals(".")) {
-					toy[qd.y][qd.x] = ".";
-					toy[qd.y][i] = type;
-					break;
+				break;
+			case 3:
+				cord[1] = y;
+				for (int i = x; i > 0; i--) {
+					if (!map[y][i-1].equals(".")) {
+						cord[0] = i;
+						if (map[y][i-1].equals("O")) {
+							cord[0] = i-1;
+							finished = true;
+							return cord;
+						}
+						break;
+					}
 				}
-			}
-			break;
+				break;
 		}
-		return false;
+		return cord;
 	}
 
 }
 
 class QueDir {
-	int x;
-	int y;
+	int rx;
+	int ry;
+	int bx;
+	int by;
 	int dir;
+	int cnt;
+	
+	QueDir(){}
+	QueDir(int rx, int ry, int bx, int by, int dir, int cnt) {
+		this.rx = rx;
+		this.ry = ry;
+		this.bx = bx;
+		this.by = by;
+		this.dir = dir;
+		this.cnt = cnt;
+	}
+	
+	
+
+	public int getRx() {
+		return rx;
+	}
+
+	public void setRx(int rx) {
+		this.rx = rx;
+	}
+
+	public int getRy() {
+		return ry;
+	}
+
+	public void setRy(int ry) {
+		this.ry = ry;
+	}
+
+	public int getBx() {
+		return bx;
+	}
+
+	public void setBx(int bx) {
+		this.bx = bx;
+	}
+
+	public int getBy() {
+		return by;
+	}
+
+	public void setBy(int by) {
+		this.by = by;
+	}
 
 	public int getDir() {
 		return dir;
@@ -165,31 +226,6 @@ class QueDir {
 
 	public void setDir(int dir) {
 		this.dir = dir;
-	}
-
-	public QueDir() {
-	}
-
-	public QueDir(int y, int x, int dir) {
-		this.x = x;
-		this.y = y;
-		this.dir = dir;
-	}
-
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
 	}
 
 }
--- src/etc/Test.java
+++ src/etc/Test.java
@@ -1,16 +1,9 @@
 package etc;
 
-import java.util.ArrayList;
-
 public class Test {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Integer>[] arr = new ArrayList[2];
-
-		ArrayList<Integer> a1 = new ArrayList<>();
-		a1.add(3);
-		
-		arr[0] = a1;
+		System.out.println(-2%2);
 	}
 }
