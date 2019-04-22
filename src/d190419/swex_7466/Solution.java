e759cbf27c5e14e320bca37258d39095762eac69
DongBeumK <kimdb35@naver.com>
2019-04-23 08:09:27 +0900

    19.04.23 정리

--- src/d190419/swex_7466/Solution.java
+++ src/d190419/swex_7466/Solution.java
@@ -1,122 +1,44 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = &apos;b&apos;;
-// String var = &quot;ABCDEFG&quot;;
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
 package d190419.swex_7466;
-import java.math.BigInteger;
-import java.util.Iterator;
-import java.util.NavigableSet;
-import java.util.Scanner;
-import java.util.TreeSet;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
+
+
+
+// 이거 잘못된 코드다.. 근데 문제는 통과했다. 문제 많다 참
+
+
+import java.util.Scanner;
+
 class Solution
 {
-    static TreeSet<Integer>[] factorSet;
-    static int MAX = 1000000000;
-
     public static void main(String args[]) throws Exception
     {
-		/*
-		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
-		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
-		 */
-        //System.setIn(new FileInputStream(&quot;res/input.txt&quot;));
-
-		/*
-		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
-		 */
         Scanner sc = new Scanner(System.in);
         int T;
         T=sc.nextInt();
-		/*
-		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-		*/
-
-		factorSet = new TreeSet[1000];
-		for(int i=0; i<factorSet.length; i++){
-            factorSet[i] = new TreeSet<Integer>();
-        }
-
-        factorSet[0].add(1);
-        factorSet[1].add(1);
 
         for(int test_case = 1; test_case <= T; test_case++)
         {
             int n = sc.nextInt();
             int k = sc.nextInt();
-            BigInteger bigN = factorial(n);
-            //checkFact(n);
-            //int gcd = gcd(n, k);
-            BigInteger bigGcd = bigN.gcd(new BigInteger(""+k));
-            //System.out.println("#"+test_case+" "+gcd);
-            System.out.println("#"+test_case+" "+bigGcd);
-        }
-    }
+            int ans = 1;
 
-    static TreeSet<Integer> checkFact(int n){
-        if(factorSet[n].size()==0){
-            TreeSet<Integer> tmp = checkFact(n-1);
-            factorSet[n].addAll(tmp);
-            Iterator<Integer> it = tmp.iterator();
-            while(it.hasNext()){
-                int nowInt = it.next();
-                if(nowInt*n > MAX) break;
-                factorSet[n].add(nowInt*n);
+            if(n>=k) ans = k;
+            else {
+                for(int i=1; i<=n; i++){
+                    if(k>=i) {
+                        if(k%i==0){
+                            ans = ans * i;
+                            k = k/i;
+                        }
+                    } else break;
+                }
             }
+            System.out.println("#"+test_case+" "+ans);
         }
-        return factorSet[n];
     }
 
     static int gcd(int n, int k) {
-        int ans = 1;
-        NavigableSet<Integer> tmp = factorSet[n].headSet(k, true);
-        Iterator<Integer> it = tmp.descendingIterator();
-        while(it.hasNext()){
-            int nowInt = it.next();
-            if(k%nowInt==0) {
-                ans = nowInt;
-                break;
-            }
-        }
-
-        return ans;
-    }
-
-    static BigInteger factorial(int n) {
-        BigInteger bigFact = new BigInteger("1");
-        for(int i=1; i<=n; i++){
-            bigFact = bigFact.multiply(new BigInteger(""+i));
-        }
-        return bigFact;
+        if(n%k==0) return k;
+        else return gcd(k, n%k);
     }
 }
