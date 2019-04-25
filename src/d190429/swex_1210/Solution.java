f0f571a71a5dfcb63c2b9e5dd34316b1b3bb5f4d
DongBeumK <kimdb35@naver.com>
2019-04-25 22:21:12 +0900

    사다리 완료

--- src/d190429/swex_1210/Solution.java
+++ src/d190429/swex_1210/Solution.java
@@ -6,12 +6,43 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-
-        for(int test_case=1; test_case<=T; test_case++){
+        for(int sldkfja=0; sldkfja<10; sldkfja++) {
+            int[][] map = new int[102][102];
             int ans = 0;
+            int iniX = 0;
+            int iniY = 0;
+            int test_case = sc.nextInt();
+            for(int i=0; i<102; i++){
+                for(int j=0; j<102; j++){
+                    if(i==0 || j==0 || i==101 || j==101) map[i][j] = 0;
+                    else {
+                        map[i][j] = sc.nextInt();
+                        if(map[i][j] == 2) {
+                            iniX = j;
+                            iniY = i;
+                        }
+                    }
+                }
+            }
+            ans = move(map, iniX, iniY);
             System.out.println("#"+test_case+" "+ans);
         }
         sc.close();
     }
+
+    static int move(int[][] map, int x, int y) {
+        while(y>=0){
+            if(y==0) {
+                return x;
+            }
+            if(map[y][x+1]!=0) {
+                while(map[y][++x+1]!=0);
+            } else if(map[y][x-1]!=0) {
+                while(map[y][--x-1]!=0);
+            }
+            y--;
+        }
+        return -1;
+    }
+
 }
--- src/d190429/swex_6782/Solution.java
+++ src/d190429/swex_6782/Solution.java
@@ -4,30 +4,27 @@ import java.util.Scanner;
 
 public class Solution {
 
-	/*
-	 * 1. 루트할 수 있으면 무조건 루트 해야한다. 안하면 손해
-	 * 2. 4나 16이나 이런거 노려볼까 했는데 걍 루트때려
-	 * 3. DP로 푸는 건 어떨까..
-	 */
-	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		
 		for(int test_case=1; test_case<=T; test_case++) {
-			int ans = 0;
-			int n = sc.nextInt();
+			long ans = 0;
+			long n = Long.parseLong(sc.next());
 			while(true) {
-				if(isTwo(n)) break;
-				
+				if(n==2) break;
+				long tmp = (long) Math.sqrt(n);
+				if(tmp*tmp == n) {
+					n = tmp;
+				} else {
+					ans += (tmp+1)*(tmp+1) - n;
+					n = tmp+1;
+				}
+				ans++;
 			}
 			System.out.println("#"+test_case+" "+ans);
 		}
 		sc.close();
 	}
 	
-	static boolean isTwo(int num) {
-		return num==2;
-	}
 }
